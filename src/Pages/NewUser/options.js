ffd826d8db2b6bdd75248592457f413fa198b7a8
Jesse Ou <jesse.ou@berkeley.edu>
2017-10-07 05:47:23 -0700

    In options.js, added the 'prefer not to say option' as empty

--- src/Pages/NewUser/options.js
+++ src/Pages/NewUser/options.js
@@ -1,15 +1,18 @@
 var sexOptions = {
+  empty: "Prefer not to say",
   male: "Male",
   female: "Female"
 }
 
 var ageOptions = {
+  empty: "Prefer not to say",
   under18: "Under 18 years",
   between18And64: "18 to 64 years",
   above64: "65 years and over"
 }
 
 var raceOptions = {
+  empty: "Prefer not to say",
   white: "White",
   black: "Black or African American",
   nativeAmerican: "American Indian and Alaska Native",
@@ -22,12 +25,14 @@ var raceOptions = {
 };
 
 var livingOptions = {
+  empty: "Prefer not to say",
   married: "In married-couple family",
   femaleHolder: "In Female householder, no husband present household",
   otherLiving: "In other living arrangement"
 }
 
 var educationOptions = {
+  empty: "Prefer not to say",
   lessThanHighSchool: "Less than high school graduate",
   highSchool: "High school graduate (includes equivalency)",
   college: "Some college or associate's degree",
@@ -35,18 +40,21 @@ var educationOptions = {
 };
 
 var workStatusOptions = {
+  empty: "Prefer not to say",
   full: "Worked full-time, year-round",
   lessFull: "Worked less than full-time, year-round",
   noWork: "Did not work"
 };
 
 var citizenshipOptions = {
+  empty: "Prefer not to say",
   native: "Native",
   foreign: "Foreign born",
   naturalized: "Naturalized citizen"
 };
 
 var disabilityOptions = {
+  empty: "Prefer not to say",
   yes: "With any disability",
   no: "No disability"
 }
