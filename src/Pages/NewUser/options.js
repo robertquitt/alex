7b20305521ef7b7487ce23c605d06173b29e8f42
Jesse Ou <jesse.ou@berkeley.edu>
2017-10-07 05:07:27 -0700

    filled in options.js with different demographic factor options that mapped the outward display of those options to the inward display of those options. Ex: asian: Asian

--- src/Pages/NewUser/options.js
+++ src/Pages/NewUser/options.js
@@ -1,19 +1,52 @@
+var sexOptions = {
+  male: "Male",
+  female: "Female"
+}
+
+var ageOptions = {
+  under18: "Under 18 years",
+  between18And64: "18 to 64 years",
+  above64: "65 years and over"
+}
+
 var raceOptions = {
-
+  white: "White",
+  black: "Black or African American",
+  nativeAmerican: "American Indian and Alaska Native",
+  asian: "Asian",
+  hawaiian: "Native Hawaiian and Other Pacific Islander",
+  otherRace: "Some other race",
+  multiRace: "Two or more races",
+  hispanic: "Hispanic or Latino origin (of any race)",
+  whiteAlone: "White alone, not Hispanic or Latino"
 };
 
-var genderOptions = {
-
-};
+var livingOptions = {
+  married: "In married-couple family",
+  femaleHolder: "In Female householder, no husband present household",
+  otherLiving: "In other living arrangement"
+}
 
 var educationOptions = {
-
+  lessThanHighSchool: "Less than high school graduate",
+  highSchool: "High school graduate (includes equivalency)",
+  college: "Some college or associate's degree",
+  bachelor: "Bachelor's degree or higher"
 };
 
 var workStatusOptions = {
-
+  full: "Worked full-time, year-round",
+  lessFull: "Worked less than full-time, year-round",
+  noWork: "Did not work"
 };
 
 var citizenshipOptions = {
-
+  native: "Native",
+  foreign: "Foreign born",
+  naturalized: "Naturalized citizen"
 };
+
+var disabilityOptions = {
+  yes: "With any disability",
+  no: "No disability"
+}
